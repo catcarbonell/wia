fce673ef72ff3b4be8226bb79badc9418b8523ab
Cathleen Carbonell <catcarbonell@gmail.com>
2015-07-22 19:39:49 -0700

    Presentation - Finalized live example

---
+++ Presentation/js/main.js
@@ -0,0 +1,28 @@
+/* 
+
+Presentation Project -- Cathleen Carbonell -- WIA 1507
+
+*/
+
+
+$( document ).ready(function() {
+
+	$('a.gallery').featherlightGallery({
+	    openSpeed: 900,
+	    previousIcon: '&#9664;',
+    	nextIcon: '&#9654;'
+	});
+
+
+
+// code to fix iframe resize from https://github.com/noelboss/featherlight/issues/52
+
+	 $.featherlight.contentFilters.iframe = {
+  		process: function(url){
+   		return $('<iframe width="600" height="400" src="' + url + '"/>')
+  }
+}
+
+
+
+});
